6951de574a8819501c3477dca9d78599b64cc697
haruto <harutoyamazaki26@gmail.com>
2026-04-28 02:44:33 +0900

    add google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import { AuthProvider } from '@/components/auth/AuthProvider';
 
+<head>
+ <meta name="google-site-verification" content="_6dVqOelkYiLj5bRALMnjPYBr2u-xN_foeQhFSZ2AeU" />
+<head>
+
 export default function RootLayout({ children }: any) {
   return (
     <html>
